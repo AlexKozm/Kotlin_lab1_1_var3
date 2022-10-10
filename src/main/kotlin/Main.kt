7f567830cf538039301f9d31d37a00d6edfd65d6
Alex Kozminykh <sa.kozminykh@gmail.com>
2022-10-11 00:01:19 +0300

    Small changes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main() {
-    val arr = arrayOf(3, 7, 8, 2, 10, 4, 6, 1, 5)
-    sort(arr)
-    arr.forEach { print("$it ") }
+    sort(arrayOf(3, 7, 8, 2, 10, 4, 6, 1, 5)).forEach { print("$it ") }.also { println() }
+    sort(emptyArray<Int>()).forEach { print("$it ") }.also { println() }
+    sort(arrayOf(100)).forEach { print("$it ") }.also { println() }
 }
 
-tailrec fun <T : Comparable<T>> sort(arr: Array<T>, len: Int = 1) {
+tailrec fun <T : Comparable<T>> sort(arr: Array<T>, len: Int = 1): Array<T> {
     var i = 0
     while (true) {
         val first = i
@@ -22,36 +22,29 @@ tailrec fun <T : Comparable<T>> sort(arr: Array<T>, len: Int = 1) {
         sortTwoParts(arr, first, secondFirst, afterLast)
         ++i
     }
-    if (2 * len >= arr.size) {
-        return
+    return if (2 * len >= arr.size) {
+        arr
     } else {
         sort(arr, 2 * len)
     }
 }
 
-fun <T : Comparable<T>>sortTwoParts(arr: Array<T>, first: Int, sfirst: Int, after_last: Int) {
+fun <T : Comparable<T>>sortTwoParts(arr: Array<T>, first: Int, secondFirst: Int, after_last: Int) {
     val tempArr = arr.copyOfRange(first, after_last)
     var i = first
-    var j = sfirst
+    var j = secondFirst
     var pos = first
-    while (i < sfirst && j < after_last) {
-        if (tempArr[i - first] <= tempArr[j - first]) {
-            arr[pos] = tempArr[i - first]
-            ++i
+    while (i < secondFirst && j < after_last) {
+        arr[pos++] = if (tempArr[i - first] <= tempArr[j - first]) {
+            tempArr[i++ - first]
         } else {
-            arr[pos] = tempArr[j - first]
-            ++j
+            tempArr[j++ - first]
         }
-        ++pos
     }
-    while (i == sfirst && j != after_last) {
-        arr[pos] = tempArr[j - first]
-        ++j
-        ++pos
+    while (i == secondFirst && j != after_last) {
+        arr[pos++] = tempArr[j++ - first]
     }
-    while (i != sfirst && j == after_last) {
-        arr[pos] = tempArr[i - first]
-        ++i
-        ++pos
+    while (i != secondFirst && j == after_last) {
+        arr[pos++] = tempArr[i++ - first]
     }
 }
